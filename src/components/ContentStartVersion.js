cbbbc390ae5da0c568d1c634e569eed19d7c7ab7
Vladimir Piniazhin <vladimir.piniazhin@metropolia.fi>
2023-11-15 11:45:28 +0200

    Second working version after merge with Augustas

--- src/components/ContentStartVersion.js
+++ src/components/ContentStartVersion.js
@@ -4,14 +4,14 @@ import "../css/content.css";
 import "./SaleContainer/saleContainer.css";
 import SaleContainer from "./SaleContainer/SaleContainer";
 import MenuMain from "./MenuMain/MenuMain";
-// import MostPopular from "./MostPopular/MostPopular";
+import MostPopular from "./MostPopular/MostPopular";
 
 export const Content = () => {
     return (
         <div className="content">
             <MenuMain />
             <SaleContainer/> 
-            
+            <MostPopular/>     
 
 
         </div>
