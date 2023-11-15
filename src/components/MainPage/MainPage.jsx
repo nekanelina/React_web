78db8a97a45d27263da282112ab1b2ec11078330
Sami-Juhani <sami.paananen@gmail.com>
2023-11-15 21:09:51 +0200

    move mainpage components to same folder

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import MenuMain from "./MenuMain/MenuMain";
+import SaleContainer from "./SaleContainer";
+import MostPopular from "./MostPopular";
+
+const MainPage = () => {
+  return (
+    <div>
+        <MenuMain />
+        <SaleContainer />
+        <MostPopular />
+    </div>
+  )
+}
+
+export default MainPage;
