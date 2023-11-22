1727c1226fb56b77de7e7decc3f6cf1c40a53fae
Sami-Juhani <sami.paananen@gmail.com>
2023-11-22 16:33:34 +0200

    fetch token on inital render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useCallback } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import { currentUser } from "./components/Header/Login";
+import { pageStates } from "./components/Content";
+import { showOnlyOnePage } from "./utils/changePageStates";
 import "./App.css";
 import "./css/style.css";
 import "./css/styleguide.css";
 
 function App() {
   console.log("Render: App");
+
+  // Authenticate user with access token
+  const authenticate = useCallback(async (accessToken) => {
+    try {
+      const response = await fetch(
+        "http://localhost:4000/api/user/authenticate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      // If access token is invalid, refresh token
+      if (response.status === 403) {
+        const { user, accessToken } = await refreshToken(
+          localStorage.getItem("refreshToken")
+        );
+        currentUser.value = user;
+        localStorage.setItem("accessToken", accessToken);
+        pageStates.value = showOnlyOnePage("mainPage");
+        return;
+      }
+
+      const json = await response.json();
+      if (response.ok) {
+        currentUser.value = json.user;
+        pageStates.value = showOnlyOnePage("mainPage");
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const refreshToken = async (refreshToken) => {
+    try {
+      const response = await fetch("http://localhost:4000/api/user/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refreshToken: refreshToken }),
+      });
+
+      const json = await response.json();
+
+      if (response.stats === 403) {
+        console.log("refresh token expired");
+        return;
+      }
+
+      if (response.ok) {
+        return json;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      authenticate(accessToken);
+    }
+  }, [authenticate]);
 
   return (
     <div className="App">
@@ -18,7 +89,7 @@ function App() {
         <NavBar />
         <Content />
         <Footer />
-      </BrowserRouter>     
+      </BrowserRouter>
     </div>
   );
 }
