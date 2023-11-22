917b9fb77ce139c0f391fb2ab268154850da5a69
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2023-11-22 18:53:29 +0200

    Footer now is adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,51 +27,53 @@ export const Footer = () => {
                 </div>
             
                 </div>
-                <div className="group-1 box">
-                    <div className="div-wrapper">
-                        <div className="text-wrapper">Shop</div>
+                    <div className="groups-wrapper">
+                        <div className="group-1 box">
+                            <div className="div-wrapper">
+                                <div className="text-wrapper">Shop</div>
+                            </div>
+                            <div className="frame">
+                                <div className="text-wrapper-4">Gift cards</div>
+                                <div className="text-wrapper-4">Site map</div>
+                                <div className="text-wrapper-4">Polka blog</div>
+                                <div className="text-wrapper-4">Login</div>
+                                <div className="text-wrapper-4">Sign in</div>
+                            </div>
+                        </div>
+                        <div className="group-2 box">
+                            <div className="group-3">
+                                <div className="text-wrapper">Sell</div>
+                            </div>
+                            <div className="frame">
+                                <div className="text-wrapper-4">Sell on Polka</div>
+                                <div className="text-wrapper-4">Teams</div>
+                                <div className="text-wrapper-4">Forums</div>
+                                <div className="text-wrapper-4">Affiliates</div>
+                            </div>
+                        </div>
+                        <div className="group-4 box">
+                            <div className="group-5">
+                                <div className="text-wrapper">About</div>
+                            </div>
+                            <div className="frame">
+                                <div className="text-wrapper-4">Polka, Inc.</div>
+                                <div className="text-wrapper-4">Policies</div>
+                                <div className="text-wrapper-4">Investors</div>
+                                <div className="text-wrapper-4">Careers</div>
+                                <div className="text-wrapper-4">Press</div>
+                            </div>
+                        </div>
+                        <div className="group-6 box">
+                            <div className="group-7">
+                                <div className="text-wrapper">Help</div>
+                            </div>
+                            <div className="frame">
+                                <div className="text-wrapper-4">Help Center</div>
+                                <div className="text-wrapper-4">Trust and safety</div>
+                                <div className="text-wrapper-4">Privacy settings</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="frame">
-                        <div className="text-wrapper-4">Gift cards</div>
-                        <div className="text-wrapper-4">Site map</div>
-                        <div className="text-wrapper-4">Polka blog</div>
-                        <div className="text-wrapper-4">Login</div>
-                        <div className="text-wrapper-4">Sign in</div>
-                    </div>
-                </div>
-                <div className="group-2 box">
-                    <div className="group-3">
-                        <div className="text-wrapper">Sell</div>
-                    </div>
-                    <div className="frame">
-                        <div className="text-wrapper-4">Sell on Polka</div>
-                        <div className="text-wrapper-4">Teams</div>
-                        <div className="text-wrapper-4">Forums</div>
-                        <div className="text-wrapper-4">Affiliates</div>
-                    </div>
-                </div>
-                <div className="group-4 box">
-                    <div className="group-5">
-                        <div className="text-wrapper">About</div>
-                    </div>
-                    <div className="frame">
-                        <div className="text-wrapper-4">Polka, Inc.</div>
-                        <div className="text-wrapper-4">Policies</div>
-                        <div className="text-wrapper-4">Investors</div>
-                        <div className="text-wrapper-4">Careers</div>
-                        <div className="text-wrapper-4">Press</div>
-                    </div>
-                </div>
-                <div className="group-6 box">
-                    <div className="group-7">
-                        <div className="text-wrapper">Help</div>
-                    </div>
-                    <div className="frame">
-                        <div className="text-wrapper-4">Help Center</div>
-                        <div className="text-wrapper-4">Trust and safety</div>
-                        <div className="text-wrapper-4">Privacy settings</div>
-                    </div>
-                </div>
 
                 
             </div>
@@ -81,10 +83,6 @@ export const Footer = () => {
                     <div className="text-wrapper-4">Privacy policy</div>
                     <div className="text-wrapper-4">Terms of use</div>
                     <div className="text-wrapper-4">Cookies</div>
-                </div>
-                <div className="frame-5">
-                    <div className="text-wrapper-6">Scroll to top</div>
-                    <ArrowTop className="arrow-top" />
                 </div>
             </div>
         </div>
