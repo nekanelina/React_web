a3f280fd9948ed6c7f83641a459f0bc79ad410cd
Sami-Juhani <sami.paananen@gmail.com>
2023-11-24 13:30:43 +0200

    make blocker a signal

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,22 +1,20 @@
+import { signal } from "@preact/signals-react";
+
 import MenuMain from "./MenuMain/MenuMain";
 import SaleContainer from "./SaleContainer";
 import MostPopular from "./MostPopular";
 
-import { pageStates } from "../Content";
-
 import "./MainPage.css";
+
+export const blockMainPage = signal(false);
 
 const MainPage = () => {
   return (
     <div className="mainpage">
+      {blockMainPage.value && <div className="mainpage-blocker"></div>}
       <MenuMain />
       <SaleContainer />
       <MostPopular />
-      {(pageStates.value.registerPage ||
-        pageStates.value.accountPage ||
-        pageStates.value.checkoutPage) && (
-        <div className="mainpage-blocker"></div>
-      )}
     </div>
   );
 };
